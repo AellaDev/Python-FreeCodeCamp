2276a9f9314246c4eec04414a3d75d2502aa04c7
AellaDev <pacitearabella02@gmail.com>
2025-11-15 17:59:08 +0800

    Spearated lesson from main and added new py file
    - new file 'fromdum.py' is from 'Beginning Programming with Python for Dummies'
    - 'main.py' is from the freecodecamp

--- main.py
+++ main.py
@@ -1,25 +1,3 @@
-# Hello I am reviewing python :> Thank you FreeCodeCamp
-# In python, there is no need to use special keywords in order to declare varables :>
-name = 'John' # This is a str. you can use '' or "" 
-age = 25 # This is an int. there is no need for quotation marks
-# Variable names can't start with a number:< you can use underscore or letter instead
-# It can only contain alphanumeric and numbers
-# Variables are case sensitive which means age is different from Age
-# You can't use reserved keywords as variable names such as if, class, def
+name = 'John'
+age = 25
 
-my_name_is = 'Aella'
-user_age = 22
-
-#DO NOT use single letter for naming unless it is for the loops
-# you see this?
-#this is a comment
-
-
-#
-print('Hellow')
-#You can also use the print function to show multiple values, or arguments, at once by separating them with commas. For example:
-print('my favorite colors are', 'blue', 'green', 'red')
-# this is print
-
-#
-#Data Types
