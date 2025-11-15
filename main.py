3548b709efda901e7f5a74d0c5d7b07b73e4a836
AellaDev <pacitearabella02@gmail.com>
2025-11-15 13:24:11 +0800

    Variables added for good readability hehe
    -followed freecodecamp

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # Hello I am reviewing python :> Thank you FreeCodeCamp
+# In python, there is no need to use special keywords in order to declare varables :>
+name = 'John' # This is a str. you can use '' or "" 
+age = 25 # This is an int. there is no need for quotation marks
+# Variable names can't start with a number:< you can use underscore or letter instead
+# It can only contain alphanumeric and numbers
+# Variables are case sensitive which means age is different from Age
+# You can't use reserved keywords as variable names such as if, class, def
+
+my_name_is = 'Aella'
+user_age = 22
+
+#DO NOT use single letter for naming unless it is for the loops
+# you see this?
+#this is a comment
