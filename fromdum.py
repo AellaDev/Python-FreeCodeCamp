32398e2e0793e372b6df5117d47b41ae8251b145
AellaDev <pacitearabella02@gmail.com>
2025-11-16 12:20:54 +0800

    added string formattin .format()

--- fromdum.py
+++ fromdum.py
@@ -1,6 +1,12 @@
-brand = 'Apple'
-exchangeRate = 1.235235245
+message1 = '{0} is easier than {1}'.format('Python', 'Java')
+message2 = '{1} is easier than {0}'.format('Python', 'Java')
+message3 = '{:10.2f} and {:d}'.format(1.234234234, 12)
+message4 = '{}'.format(1.234234234)
 
-message = 'The price of this %s laptop is %d USD and the exchange rate is %4.2f USD to 1 EUR' %(brand, 1299, exchangeRate)
+print(message1)
 
-print(message)
+print(message2)
+
+print(message3)
+
+print(message4)
