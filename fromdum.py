ea3c1ef306f48f58e219a9fe1567a4caad316872
AellaDev <pacitearabella02@gmail.com>
2025-11-16 18:41:12 +0800

    I added other string formatting as well
    added % string formatting to practice

--- fromdum.py
+++ fromdum.py
@@ -3,6 +3,12 @@ message2 = '{1} is easier than {0}'.format('Python', 'Java')
 message3 = '{:10.2f} and {:d}'.format(1.234234234, 12)
 message4 = '{}'.format(1.234234234)
 
+name = 'Arabella'
+age = 22
+techAge = 22.916666666
+
+message5 = 'I am %s and I am %d years old. Technically %0.2f years old'%(name, age, techAge)
+
 print(message1)
 
 print(message2)
@@ -10,3 +16,5 @@ print(message2)
 print(message3)
 
 print(message4)
+
+print(message5)
