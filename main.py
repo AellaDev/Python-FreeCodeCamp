17112675ebd56a9b9589fc76e37424f3bcfa8c3e
AellaDev <pacitearabella02@gmail.com>
2025-11-17 13:48:20 +0800

    added various data types
    Many other programming languages group data types broadly as either primitive or reference types. Primitive types are simple and immutable, meaning they can't be changed once declared. Reference types can hold multiple values, and are either mutable or immutable. But Python doesn't draw a hard line between those two groups. Instead, all data gets treated as objects, and some objects are immutable while others are mutable.

--- main.py
+++ main.py
@@ -1,4 +1,43 @@
-name = 'John'
-age = 25
+# Integer: A whole number without decimals, for example, 10 or -5.
+integerVar = 10
+print('Integer = ', integerVar)
 
-print(name)
+# Float: A number with a decimal point, like 4.41 or -0.4.
+floatVar = 4.50
+print('Float = ', floatVar)
+
+# Complex: A number with a real and imaginary part, like 6 + 7j.
+complexVar = 3 + 4j
+print('Complex = ', complexVar)
+
+# String: A sequence of characters enclosed in single or double quotation marks like 'Hello world!'.
+stringVar = 'Hello'
+print('String = ', stringVar)
+
+# Boolean: A true or false type, written as True or False.
+booleanVar = True
+print('Boolean = ', booleanVar)
+
+# Set: An unordered collection of unique elements, like {4, 2, 0}.
+setVar = {7, 8, 9}
+print('Set = ', setVar)
+
+# Dictionary: A collection of key-value pairs enclosed in curly braces, like {'name': 'John Doe', 'age': 28}.
+dictionaryVar = {'name': 'Alice', 'age': 25}
+print('Dictionary = ', dictionaryVar)
+
+# Tuple: An immutable ordered collection, enclosed in brackets, like (7, 8, 4).
+tupleVar = (7, 8, 9)
+print('Tuple = ', tupleVar)
+
+# Range: A sequence of numbers, often used in loops, for example, range(5).
+rangeVar = range(5)
+print('Range = ', rangeVar)
+
+# List: An ordered collection of elements that supports different data types.
+listVar = [22, 'Hello World', 3.14, True]
+print('List = ', listVar)
+
+# None: A special value that represents the absence of a value.
+noneVar = None
+print('None = ', noneVar)
