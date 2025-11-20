8ba5064af92adc34d7a6690d4b260d1d7d2ea73b
AellaDev <pacitearabella02@gmail.com>
2025-11-20 09:09:23 +0800

    print(): formatting
    bruh completely forgot about it
    when you format shoi
     just use .format()
     also use {index:format}
     s d f

--- fromdum.py
+++ fromdum.py
@@ -3,6 +3,7 @@
 fir = input('Enter first number: ')
 sec = input('Enter Second number: ')
 
+print('First Number: {0:s} + Second Number: {1:s}'.format(fir, sec))
 '''
 input() will always store your input as STRING
 so if you want it to be math based
