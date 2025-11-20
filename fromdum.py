a7a0f21a757eafa8eed9738c2ee541e6886bed55
AellaDev <pacitearabella02@gmail.com>
2025-11-20 12:21:09 +0800

    calculator: def function

--- fromdum.py
+++ fromdum.py
@@ -10,24 +10,37 @@ Welcome to AellaDev's Calculator!
 ''')
 
 choiceofMethod = input('Choose a number from the menu: ')
+if choiceofMethod not in ['1', '2', '3', '4']:
+    print('Please choose from the menu. Number only.')
+    exit()
+else:
+    x = input('Enter X: ')
+    y = input('Enter Y: ')
 
-x = input('Enter X: ')
-y = input('Enter Y: ')
+def operations(x, y):
+    x = int(x)
+    y = int(y)
 
-x = int(x)
-y = int(y)
+    if choiceofMethod == '1':
+        ans = x + y
+    elif choiceofMethod == '2':
+        ans = x - y
+    elif choiceofMethod == '3':
+        ans = x / y
+    elif choiceofMethod == '4':
+        ans = x * y
+
+    return ans
 
 if choiceofMethod == '1':
-    ans = x + y
-    print('The {0:s} is: '.format('SUM', 'DIFFERENCE', 'QUOTIENT', 'PRODUCT'), ans)
+    ansSum = operations(x, y)
+    print('The {0:s} is: '.format('SUM', 'DIFFERENCE', 'QUOTIENT', 'PRODUCT'), ansSum)
 elif choiceofMethod == '2':
-    ans = x - y
-    print('The {1:s} is: '.format('SUM', 'DIFFERENCE', 'QUOTIENT', 'PRODUCT'), ans)
+    ansDif = operations(x, y)
+    print('The {1:s} is: '.format('SUM', 'DIFFERENCE', 'QUOTIENT', 'PRODUCT'), ansDif)
 elif choiceofMethod == '3':
-    ans = x/y
-    print('The {2:s} is: '.format('SUM', 'DIFFERENCE', 'QUOTIENT', 'PRODUCT'), ans)
+    ansQuo = operations(x, y)
+    print('The {2:s} is: '.format('SUM', 'DIFFERENCE', 'QUOTIENT', 'PRODUCT'), ansQuo)
 elif choiceofMethod == '4':
-    ans = x*y
-    print('The {3:s} is: '.format('SUM', 'DIFFERENCE', 'QUOTIENT', 'PRODUCT'), ans)
-else:
-    print('Please choose from the menu. Number only.')
+    ansPro = operations(x, y)
+    print('The {3:s} is: '.format('SUM', 'DIFFERENCE', 'QUOTIENT', 'PRODUCT'), ansPro)
