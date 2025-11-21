2784a728dd48b88ff0e38c1ee605c90434759ff5
AellaDev <pacitearabella02@gmail.com>
2025-11-22 07:50:01 +0800

    this is a BREAK

--- fromdum.py
+++ fromdum.py
@@ -1,16 +1,7 @@
-# While loop
+## Break
 
-'''
-while condition is true:
-     do A
-'''
-
-counter = 5
-'''
-Most of the time you need to declare the variable first before
-'''
-while counter > 0:
-    counter = counter - 1 # this is how you make sure that the counter doesn't stay at 5
-    print('Counter: ', counter)
-   
-
+j = 0
+for i in range(5):
+    j = j + 2
+    print('i = ', i, 'j = ', j)
+    if j == 6 : break
