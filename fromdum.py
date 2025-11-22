728524f59aeeb29774b76f24b089109db8c4ef49
AellaDev <pacitearabella02@gmail.com>
2025-11-22 13:16:22 +0800

    Continue

--- fromdum.py
+++ fromdum.py
@@ -1,50 +1,42 @@
-## Calculator
+j = 0
+while j < 5:
+    print('j = ', j)
+    if j == 3:
+        j += 1
+        continue
+    print('I will skip over 3')
+    j += 1
 
-while True:
-     print('''
-     Welcome to AellaDev's Calculator!
-               Menu
-     1. Addition
-     2. Subtraction
-     3. Division
-     4. Multiplication
-     ''')
 
-     choiceofMethod = input('Choose a number from the menu: ')
-     if choiceofMethod not in ['1', '2', '3', '4']:
-          print('Please choose from the menu. Number only.')
-          cont = input('Press \'n\' to continue: ')
-          if cont == 'n': print('Continuing ...')
-          else: break
-     else:
-          x = input('Enter X: ')
-          y = input('Enter Y: ')
+    '''
+    This one is a bit hard to understand since the book I was reading is older python 3
+    so now the continue is way more harder to understand to me
+    but basically
+    when the computer reads the word 'continue'
+    it ends the iteration not the loop
+    so it will just skip the iteration nd 'continue'
+    is the last thing it will read before repeating
 
-          x = int(x)
-          y = int(y)
+    the reason why ther is a j+=1 inside the if is because 
+    once the code reaches the if
+    and j == 3 is indeed true
+    then it will read the if code block
+    nd since it has 'continue'
+    it will skip the bottom code which holds the increment
 
-          ansSum = x + y
-          ansDif = x - y
-          ansQuo = x / y
-          ansPro = x * y
+    so qhat will happen is that it will add 1 to the j which is now 3
+    then end iteration
+    then loop again 
+    
+    this one is a bit hard to understand my head is bursting
+    but why does the if statement knows its a 3
+    well here it is
+    when you are starting the loop it starts at 0
+    0 not 3 skip if then + 1
+    1 not 3 skip if then + 1
+    2 not 3 skip if then + 1
+    3 is 3 enter if then + 1
+    etc
 
-          if choiceofMethod == '1':
-               print('The {0:s} is: '.format('SUM', 'DIFFERENCE', 'QUOTIENT', 'PRODUCT'), ansSum)
-               cont = input('Press \'n\' to continue: ')
-               if cont == 'n': print('Continuing ...')
-               else: break
-          elif choiceofMethod == '2':
-               print('The {1:s} is: '.format('SUM', 'DIFFERENCE', 'QUOTIENT', 'PRODUCT'), ansDif)
-               cont = input('Press \'n\' to continue: ')
-               if cont == 'n': print('Continuing ...')
-               else: break
-          elif choiceofMethod == '3':
-               print('The {2:s} is: '.format('SUM', 'DIFFERENCE', 'QUOTIENT', 'PRODUCT'), ansQuo)
-               cont = input('Press \'n\' to continue: ')
-               if cont == 'n': print('Continuing ...')
-               else: break
-          elif choiceofMethod == '4':
-               print('The {3:s} is: '.format('SUM', 'DIFFERENCE', 'QUOTIENT', 'PRODUCT'), ansPro)
-               cont = input('Press \'n\' to continue: ')
-               if cont == 'n': print('Continuing ...')
-               else: break
+    my brain hurts
+    '''
